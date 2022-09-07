3c08fd5dff1cd36ca3fc80a71e8faeb60e3b8521
akentominas <akentominas@gmail.com>
2022-09-07 09:01:28 +0300

    adding check for pub key existance

--- support-files/env.py
+++ support-files/env.py
@@ -1,4 +1,5 @@
 import os
+from os.path import exists
 
 
 class AwsAccessKeyMissing(Exception):
@@ -9,6 +10,10 @@ class AwsSecretKeyMissing(Exception):
     pass
 
 
+class SshKeyMissing(Exception):
+    pass
+
+
 def aws_access_key():
     aws_access_key = os.environ.get("AWS_ACCESS_KEY")
 
@@ -16,6 +21,7 @@ def aws_access_key():
         raise AwsAccessKeyMissing(
             "AWS_ACCESS_KEY env variable is missing, please modify and run the support-files/env_variables.sh script"
         )
+    print("AWS_ACCESS_KEY is populated")
 
 
 def aws_secret_key():
@@ -25,7 +31,19 @@ def aws_secret_key():
         raise AwsSecretKeyMissing(
             "AWS_SECRET_KEY env variable is missing, please modify and run the support-files/env_variables.sh script"
         )
+    print("AWS_SECRET_KEY is populated")
+
+
+def ssh_key_missing():
+    ssh_key_missing = exists("../keys/aws-key.pub")
+
+    if not ssh_key_missing:
+        raise SshKeyMissing(
+            "aws-key.pub is not populated. Please run the command specified in the README file and place it inder the keys directory"
+        )
+    print("aws-key.pub is populated")
 
 
 aws_access_key()
 aws_secret_key()
+ssh_key_missing()
